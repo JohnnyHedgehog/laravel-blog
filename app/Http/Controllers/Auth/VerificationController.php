bd2f0ad6d40776358bfd1c1a90873e9184de316f
JohnnyHedgehog <alexey.virin@gmail.com>
2022-05-22 23:03:31 +0300

    Lesson 28 - Email Verification

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -6,8 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\VerifiesEmails;
 
-class VerificationController extends Controller
-{
+class VerificationController extends Controller {
     /*
     |--------------------------------------------------------------------------
     | Email Verification Controller
@@ -27,14 +26,14 @@ class VerificationController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
+    // protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
-    {
+    public function __construct() {
         $this->middleware('auth');
         $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
